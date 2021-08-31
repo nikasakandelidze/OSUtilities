036a557b483ad35c4a39fa42b7e00064bef13ff6
sakana <nikasakana@gmail.com>
2021-08-31 17:00:01 +0400

    finished cd command

--- shell/shell.c
+++ shell/shell.c
@@ -7,8 +7,10 @@
 #include <string.h>
 #include "tokenizer.h"
 
+#define NUMBER_OF_COMMANDS 2
+
 typedef void* (*command_ptr)(struct tokens* tokens);
-  
+
 void* pwd_command(struct tokens* tokens){
     char cwd[1024];
 	if (getcwd(cwd, sizeof(cwd)) == NULL) {
@@ -19,16 +21,41 @@ void* pwd_command(struct tokens* tokens){
     return NULL;
 }
 
+
+void cd_to_path(char* path){
+      if (chdir(path) == 0){
+            pwd_command(NULL);
+      }else{
+         perror("chdir() error");
+      }
+}
+
+
+
+void* cd_command(struct tokens* tokens){
+    if(get_number_of_tokens(tokens) == 1){
+        cd_to_path("/home");
+    }else if(tokens -> log_len == 2){
+        cd_to_path(get_nth_token(1,tokens));
+    }else{
+        printf("Incorrect invocation of cd command\n");
+    }
+}
+
 struct fn_map{
     char* title;
     command_ptr cmd_function;                        
 };
 
-
-struct fn_map cmd_functions[] = { { .title = "pwd", .cmd_function = &pwd_command} };  
+struct fn_map cmd_functions[] = { { .title = "pwd", .cmd_function = &pwd_command}, { .title = "cd", .cmd_function = &cd_command} };  
 
 command_ptr get_function(char* cmd_title){
-    return cmd_functions[0].cmd_function;
+    for(int i=0; i<NUMBER_OF_COMMANDS; i++){
+        if(strcmp(cmd_title, cmd_functions[i].title) == 0){
+            return cmd_functions[i].cmd_function;
+        }
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -36,9 +63,15 @@ int main(int argc, char *argv[]) {
     static char line[4096];
     printf(">");
     while(fgets(line, 4096, stdin)){
-        struct tokens* token = tokenize(line);
-        get_function(line)(token); 
-        destroy_tokens(token);
+        line[strcspn(line, "\n")] = 0;
+        struct tokens* tokens = tokenize(line);
+        command_ptr fnc = get_function(get_nth_token(0, tokens)); 
+        if(fnc == NULL){
+            printf("Couldn't invoke function %s\n", line);
+        }else{
+            fnc(tokens);
+        }
+        destroy_tokens(tokens);
         printf(">");
     }
     return 0;
